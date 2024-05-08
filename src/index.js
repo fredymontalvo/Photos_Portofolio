24220a6281a08e614a40f73a04782d041c6d7b8d
fredymontalvo <fredualdo@gmail.com>
2024-05-08 16:21:20 +0200

    responsive main menu

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@ function resetForm() {
   document.getElementById("form").reset();
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+  const galleryPlus = document.querySelector('header .nav-bar ul li:nth-child(2) a');
+  const subMenu = document.querySelector('header .nav-bar ul li:nth-child(2) ul');
+
+  galleryPlus.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita que el enlace se siga al hacer clic
+
+    // Alternar la clase 'show' en el submenú para mostrar u ocultar
+    subMenu.classList.toggle('show');
+  });
+});
